d8d1822a8fc24b34ed09492f685736c3367f0da6
Aziz Shamim <azizshamim@gmail.com>
2012-05-29 15:12:13 -0500

    add puppet master config change and restart on hostname change (for appliances)

--- manifests/agent/config.pp
+++ manifests/agent/config.pp
@@ -7,10 +7,22 @@ class puppet::agent::config(
     group => $puppet::params::pt_puppet_group,
     mode  => '0644',
   }
+  Exec {
+    path    => '/bin:/sbin:/usr/bin:/usr/sbin',
+  }
 
+  #exec{'clean_ssl':
+  #  command => "rm -rf ssl/",
+  #  cwd     => "${puppet::params::pt_puppet_confdir}",
+  #  unless  => "test -f ${puppet:params::pt_puppet_confidr}/ssl/certs/${fqdn}.pem",
+  #  refreshonly => true,
+  #  notify      => Service['pe-puppet'],
+  #}
   file { "${puppet::params::pt_puppet_confdir}/puppet.conf":
     ensure  => file,
     content => template('puppet/puppet/puppet_agent.conf.erb'),
+    notify      => Service['pe-puppet'],
+    #  notify  => Exec['clean_ssl'],
   }
   user { $puppet::params::pt_puppet_user:
     ensure  => present,
